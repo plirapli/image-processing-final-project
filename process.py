a064f9e50830434e2b0b480d6dbbb922e8bbafb4
rafliyulistiawan <rafliyulistiawan@gmail>
2023-12-29 00:11:48 +0700

    fix high boost

--- process.py
+++ process.py
@@ -18,7 +18,7 @@ def grayscaling(image):
     image = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
     return image
 
-def unsharpMaskingAndHighboostFiltering(img):
+def highboostFiltering(img):
     k = 8
     img = img.reshape(img.shape[0], img.shape[1], 1)
     # Kedua, ini ceritanya diblur pake gaussian blur
@@ -29,19 +29,6 @@ def unsharpMaskingAndHighboostFiltering(img):
     # Terakhir, gambar original tadi ditambah sama gambar hasil pengurangannya
     final = img + k * mask
     return final
-
-def prep_image(image, save_path=None):
-    img = grayscaling(image)
-    # img = equalizing(img)
-    # img = high_boost(img)
-    img = cv2.GaussianBlur(img, (5,5), 0)
-    img = unsharpMaskingAndHighboostFiltering(img)
-    img = cv2.medianBlur(img, 5)
-
-    # Save the processed image
-    if save_path:
-        cv2.imwrite(save_path, img)
-    return img
 
 def prep_image_1(image, save_path=None):
     img = grayscaling(image)
@@ -60,7 +47,7 @@ def prep_image_2(image, save_path=None):
     return img
 
 def prep_image_3(image, save_path=None):
-    img = unsharpMaskingAndHighboostFiltering(image)
+    img = highboostFiltering(image)
 
     # Save the processed image
     if save_path:
